d42a1a7ad7e29ebef9298a248a43bad78f55c166
saratoga118 <saratoga118@gmail.com>
2021-04-20 17:18:16 +0200

    rid removal of ill-formed arg names
    Complete rule removal in case of ill-formed arg names.

--- crs_secr_update1.py
+++ crs_secr_update1.py
@@ -10,7 +10,7 @@ import logging
 parser = argparse.ArgumentParser(
     prog='crs_secr_update1',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--maxargs', type=int, default=5, help='max number of args for target id updated')
+parser.add_argument('--maxargs', type=int, default=15, help='max number of args for target id updated')
 parser.add_argument('--debug', action="store_true", help='Turn on debugging')
 parser.add_argument('--id-start', type=int, default=12001, help='Starting id for white list rules')
 parser.add_argument('file', nargs='*', help='file names')
@@ -23,6 +23,9 @@ logging.debug("maxargs is %i" % args.maxargs)
 
 ms_re = re.compile(r'\bModSecurity:\s')
 # at_re = re.compile(r"\s+(?:at\s+([^.]+)|in\s+(\S+))")
+re_well_formed_args = re.compile(r"^[\w_-]+([:\w_-]+)?$")
+
+wl_rule_incr = 10
 
 # Style with "against variable":
 
@@ -42,8 +45,8 @@ p_re = re.compile(r"^(/[^/]+)/")
 
 
 def base_path(s):
-    m = p_re.search(s)
-    return m.group(1) if m else '/'
+    m_p = p_re.search(s)
+    return m_p.group(1) if m_p else '/'
 
 
 def base_path_list(pl):
@@ -58,20 +61,20 @@ def base_path_list(pl):
 def parse_line(modsec_line):
     res = {}
     for at_re in at_re_list:
-    m = at_re.search(modsec_line)
-    if m:
-        res["_at"] = m.group(1)
-        while modsec_line:
-            m = fld_re.search(modsec_line)
-            if m:
-                fld_name, contents, rest = m.groups()
-                # res.setdefault(fld_name, set())
-                if fld_name not in res:
-                    res[fld_name] = set()
-                res[fld_name].add(contents)
-                modsec_line = rest
-            else:
-                modsec_line = ''
+        m_at = at_re.search(modsec_line)
+        if m_at:
+            res["_at"] = m_at.group(1)
+            while modsec_line:
+                m_at = fld_re.search(modsec_line)
+                if m_at:
+                    fld_name, contents, rest = m_at.groups()
+                    # res.setdefault(fld_name, set())
+                    if fld_name not in res:
+                        res[fld_name] = set()
+                    res[fld_name].add(contents)
+                    modsec_line = rest
+                else:
+                    modsec_line = ''
     return res
 
 
@@ -95,29 +98,42 @@ for input_filename in args.file:
                     for uri in r["uri"]:
                         at_list[rid]["uri"].add(uri)
 
-s_upd = []
-s_whitelist = []
+l_upd = []
+d_update = {}
+l_whitelist = []
+s_disabled = set()
 
 num_re = re.compile(r"^\d+$")
 
 pfx_list = {}
+
+"""r_comment = [
+    ("# Rule id %s - %s" % (rid, at_list[rid]["msg"])),
+    ("# 'at' list: %s" % str(at_list[rid]["_at"])),
+    # ("# uri list: %s" % str(at_list[rid]["uri"]))
+    ("# base path list: %s" % base_path_list(at_list[rid]["uri"]))
+]
+l_upd.extend(r_comment)
+"""
+
 for rid in sorted(at_list):
     num_match = num_re.search(rid)
     if num_match:
         if len(at_list[rid]["_at"]) < args.maxargs:
-            r_comment = [
-                ("# Rule id %s - %s" % (rid, at_list[rid]["msg"])),
-                ("# 'at' list: %s" % str(at_list[rid]["_at"])),
-                # ("# uri list: %s" % str(at_list[rid]["uri"]))
-                ("# base path list: %s" % base_path_list(at_list[rid]["uri"]))
-            ]
-            s_upd.extend(r_comment)
-            for arg in sorted(at_list[rid]['_at']):
-                s_upd.extend(['SecRuleUpdateTargetById %s "!%s"' % (rid, arg)])
-            s_upd.extend([""])
+            for at in sorted(at_list[rid]["_at"]):
+                m = re_well_formed_args.search(at)
+                if m:
+                    d_update.setdefault(rid, set())
+                    d_update[rid].add(at)
+                else:
+                    """ We want to get rid of strange parameter names like 
+                    FILES:%27Non-ASCII%20in%20Title%20%EF%80%A1%20blabla%20attaboy-en%20.pdf
+                    """
+                    logging.debug("Disabling rule %s due to ill-formed argument: %s" % (rid, at))
+                    s_disabled.add(rid)
         else:
-            logging.debug('max_args exceeded for rule id %s: List of ModSecurity "at" %s' %
-                          (rid, str(sorted(at_list[rid]["_at"]))))
+            logging.debug('max_args exceeded for rule id %s (%i matches): List of ModSecurity "at" %s' %
+                          (rid, len(at_list[rid]["_at"]), str(sorted(at_list[rid]["_at"]))))
             path_prefix = base_path_list(at_list[rid]["uri"])
             logging.debug('Path prefixes for rule id %s: %s' % (rid, str(path_prefix)))
             for path in path_prefix:
@@ -126,23 +142,51 @@ for rid in sorted(at_list):
     else:
         logging.warning("Ignoring non-numeric rule id '%s'" % rid)
 
+# Determine ruleid's that occur in a large number of the path prefixes
+paths = sorted(list(pfx_list))
+num_paths = len(paths)
+rid_paths = {}
+max_rule_path_mentions_factor = 0.7
+
+for path in paths:
+    for rid in pfx_list[path]:
+        rid_paths.setdefault(rid, set())
+        rid_paths[rid].add(path)
+max_rule_path_mentions = int(max_rule_path_mentions_factor * num_paths)
+for rid in sorted(rid_paths):
+    if len(rid_paths[rid]) > max_rule_path_mentions:
+        logging.debug("rid %s occurs in %i of %i URI paths - disabling entire rule" %
+                      (rid, len(rid_paths[rid]), len(paths)))
+        s_disabled.add(rid)
+        for path in pfx_list:
+            pfx_list[path].remove(rid)
+
 wl_rule_id = args.id_start
 for path in sorted(pfx_list):
     ctl_list = "," . join(["\\\n    ctl:ruleRemoveById=%s" % i for i in sorted(pfx_list[path])])
-    s_whitelist.extend(['SecRule REQUEST_URI "@beginsWith %s" "id:\'%i\',phase:1,t:none,pass,nolog,%s"\n' %
-                        (path, wl_rule_id, ctl_list)])
-    wl_rule_id += 1
+    if ctl_list:
+        l_whitelist.extend(['SecRule REQUEST_URI "@beginsWith %s" "id:\'%i\',phase:1,t:none,pass,nolog,%s"\n' %
+                            (path, wl_rule_id, ctl_list)])
+        wl_rule_id += wl_rule_incr
 
 print('')
 print("# >>>>> White list <<<<<<")
 print("# to be inserted in config file *before* ModSecurity rule file includes\n")
 
-for line in sorted(s_whitelist):
+for line in sorted(l_whitelist):
     print(line)
 print('')
 
 print("# >>>>> Excludes <<<<<<")
 print("# to be inserted in config file *after* ModSecurity rule file includes")
-for line in s_upd:
-    print(line)
+
+print("# Disabled secrules")
+for rid in sorted(s_disabled):
+    print("SecRuleRemoveById %s" % rid)
+print('')
+
+print("# Updated secrules")
+for rid in sorted(l_upd):
+    for at in sorted(d_update[rid]):
+        print('SecRuleUpdateTargetById %s "!%s"' % (rid, at))
 print('')
