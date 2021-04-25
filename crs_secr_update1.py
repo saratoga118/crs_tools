fcf7965d8ef2f60835811906a7ccbf52bf408be6
Francois Perruchoud <fperruchoud@gmail.com>
2021-04-25 14:57:24 +0200

    Split parsing to modsecurity_lines.py. Added option --min-arg-matches

--- crs_secr_update1.py
+++ crs_secr_update1.py
@@ -6,17 +6,23 @@ import logging
 
 
 # from typing import List, Any, Union
+from modsecurity_lines import parse_line, set_parse_method
 
 parse_method_selector = ["re", "str"]
+
+
 def parse_method(s):
     if s not in parse_method_selector:
         raise ValueError("Parse method must be in: "+str(parse_method_selector))
     return s
 
+
 parser = argparse.ArgumentParser(
     prog='crs_secr_update1',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--max-rule-vars', type=int, default=15, help='max number of args for target id updated')
+parser.add_argument('--min-arg-matches', type=int, default=5,
+                    help='minimum number of hits for a given argument of a rule to be considered for rule udates')
 parser.add_argument('--debug', action="store_true", help='Turn on debugging')
 parser.add_argument('--id-start', type=int, default=12001, help='Starting id for white list rules')
 parser.add_argument('--parse-method', type=parse_method, default="re", help='Parsing method: "str" or "re"')
@@ -33,20 +39,7 @@ re_well_formed_args = re.compile(r"^[\w_-]+([:\w_-]+)?$")
 
 wl_rule_incr = 10
 
-# Style with "against variable":
 
-# Modsecurity: Rule Id: 942421 phase: 2 * Match, but no disruptive action: ModSecurity: Warning. Matched "Operator
-# `Rx' with parameter `((?:[~!@#\$%\^&\*\(\)\-\+=\{\}\[\]\|:;\"'\xc2\xb4\xe2\x80\x99\xe2\x80\x98`<>][^~!@#\$%\^&\*\(
-# \)\-\+=\{\}\[\]\|:;\"'\xc2\xb4\xe2\x80\x99\xe2\x80\x98`<>]*?){3})' against variable `REQUEST_COOKIES:DEA_vertrag' (
-# Value: `J17106||en|||||100000000||||8515||Response_18f1ca847fe1aa7cc4fcbd24624ffac98f685f70|||||||||||nnnnoo (52
-# characters omitted)' ) [file ...
-
-at_re_list = [
-    re.compile(r'\s+at\s+(.*?)\.\s+(\[.*)'),
-    re.compile(r"\s+against\s+variable\s+`([^']+)(.*)")
-]
-
-fld_re = re.compile(r"\[(\w+)\s+\"([^\"]+)(.*)")
 p_re = re.compile(r"^(/[^/]+)/")
 
 
@@ -64,78 +57,7 @@ def base_path_list(pl):
     return res
 
 
-def parse_fields_re(line):
-    res = {}
-    while line:
-        m_at = fld_re.search(line)
-        if m_at:
-            fld_name, contents, rest = m_at.groups()
-            # res.setdefault(fld_name, set())
-            if fld_name not in res:
-                res[fld_name] = set()
-            res[fld_name].add(contents)
-            line = rest
-        else:
-            line = ''
-    return res
-
-def parse_fields_str(line):
-    res = {}
-    while line:
-        tok_start = line.find("[")
-        if tok_start < 0:
-            return res
-        tok_end = line.find("]")
-        if tok_end < 0:
-            return res
-        b = line[tok_start + 1:tok_end]
-        li = b.split(" ", 1)
-        if len(li) == 2:
-            fld, val = li
-            if val[0] == '"':
-                val = val[1:-1]
-            if fld not in res:
-                res[fld] = set()
-            res[fld].add(val)
-        else:
-            # logging.warning("unexpected number of fields: " + str(li))
-            pass
-        line = line[1+tok_end:]
-    return res
-
-
-if args.parse_method == "re":
-    parse_fields = parse_fields_re
-else:
-    parse_fields = parse_fields_str
-logging.debug("parse_fields method: "+str(parse_fields))
-
-"""
-Which method is faster: 
-for f in str re; do   outf=$f.res; rm -f $outf; time ./crs_secr_update1.py --debug  --max 20 --parse=$f  bigfile  >$outf 2>&1 ;done
-
-real	0m35.022s
-user	0m34.569s
-sys	0m0.301s
-
-real	0m30.690s
-user	0m30.381s
-sys	0m0.249s
-"""
-
-
-def parse_line(modsec_line):
-    res = {}
-    for at_re in at_re_list:
-        m_at = at_re.search(modsec_line)
-        if m_at:
-            res["_at"] = m_at.group(1)
-            r = parse_fields(m_at.group(2))
-            if r:
-                for i in r:
-                    res[i] = r[i]
-    return res
-
+set_parse_method(args.parse_method)
 
 at_list = {}
 rid_msg = {}
@@ -147,19 +69,21 @@ for input_filename in args.file:
                 for rid in r["id"]:
                     at_list.setdefault(rid, {
                         "msg": "",
-                        "_at": set(),
-                        "uri": set()
+                        "_at": {},
+                        "uri": {}
                     }
                     )
                     if "msg" in r:
                         at_list[rid]["msg"] = list(r["msg"])[0]
                         rid_msg[rid] = r["msg"]
                     if "_at" in r:
-                        at_list[rid]["_at"].add(r["_at"])
+                        at_list[rid]["_at"].setdefault(r["_at"], 0)
+                        at_list[rid]["_at"][r["_at"]] += 1
                     if "tag" in r:
                         pass
                     for uri in r["uri"]:
-                        at_list[rid]["uri"].add(uri)
+                        at_list[rid]["uri"].setdefault(uri, 0)
+                        at_list[rid]["uri"][uri] += 1
 
 d_update = {}
 l_whitelist = []
@@ -169,7 +93,8 @@ num_re = re.compile(r"^\d+$")
 
 pfx_list = {}
 
-"""r_comment = [
+"""
+r_comment = [
     ("# Rule id %s - %s" % (rid, at_list[rid]["msg"])),
     ("# 'at' list: %s" % str(at_list[rid]["_at"])),
     # ("# uri list: %s" % str(at_list[rid]["uri"]))
@@ -183,16 +108,20 @@ for rid in sorted(at_list):
     if num_match:
         if len(at_list[rid]["_at"]) < args.max_rule_vars:
             for at in sorted(at_list[rid]["_at"]):
-                m = re_well_formed_args.search(at)
-                if m:
-                    d_update.setdefault(rid, set())
-                    d_update[rid].add(at)
+                if at_list[rid]["_at"][at] >= args.min_arg_matches:
+                    m = re_well_formed_args.search(at)
+                    if m:
+                        d_update.setdefault(rid, set())
+                        d_update[rid].add(at)
+                    else:
+                        """ We want to get rid of strange parameter names like 
+                        FILES:%27Non-ASCII%20in%20Title%20%EF%80%A1%20blabla%20attaboy-en%20.pdf
+                        """
+                        logging.debug("Disabling rule %s due to ill-formed argument: %s" % (rid, at))
+                        s_disabled.add(rid)
                 else:
-                    """ We want to get rid of strange parameter names like 
-                    FILES:%27Non-ASCII%20in%20Title%20%EF%80%A1%20blabla%20attaboy-en%20.pdf
-                    """
-                    logging.debug("Disabling rule %s due to ill-formed argument: %s" % (rid, at))
-                    s_disabled.add(rid)
+                    logging.debug("rid '%s', arg '%s': Ingoring due to insufficient hits (%i)" %
+                                  (rid, at, at_list[rid]["_at"][at]))
         else:
             logging.debug('max_rule_vars exceeded for rule id %s (%i matches): List of ModSecurity "at" %s' %
                           (rid, len(at_list[rid]["_at"]), str(sorted(at_list[rid]["_at"]))))
@@ -260,4 +189,5 @@ for rid in sorted(d_update):
     print_rid_msg(rid)
     for at in sorted(d_update[rid]):
         print('SecRuleUpdateTargetById %s "!%s"' % (rid, at))
+    print("")
 print('')
---
+++ modsecurity_lines.py
@@ -0,0 +1,81 @@
+import logging
+import re
+
+fld_re = re.compile(r"\[(\w+)\s+\"([^\"]+)(.*)")
+
+# Style with "against variable":
+
+# Modsecurity: Rule Id: 942421 phase: 2 * Match, but no disruptive action: ModSecurity: Warning. Matched "Operator
+# `Rx' with parameter `((?:[~!@#\$%\^&\*\(\)\-\+=\{\}\[\]\|:;\"'\xc2\xb4\xe2\x80\x99\xe2\x80\x98`<>][^~!@#\$%\^&\*\(
+# \)\-\+=\{\}\[\]\|:;\"'\xc2\xb4\xe2\x80\x99\xe2\x80\x98`<>]*?){3})' against variable `REQUEST_COOKIES:DEA_vertrag' (
+# Value: `J17106||en|||||100000000||||8515||Response_18f1ca847fe1aa7cc4fcbd24624ffac98f685f70|||||||||||nnnnoo (52
+# characters omitted)' ) [file ...
+
+at_re_list = [
+    re.compile(r'\s+at\s+(.*?)\.\s+(\[.*)'),
+    re.compile(r"\s+against\s+variable\s+`([^']+)(.*)")
+]
+
+
+def parse_line(modsec_line):
+    res = {}
+    for at_re in at_re_list:
+        m_at = at_re.search(modsec_line)
+        if m_at:
+            res["_at"] = m_at.group(1)
+            r = parse_fields(m_at.group(2))
+            if r:
+                for i in r:
+                    res[i] = r[i]
+    return res
+
+
+def parse_fields_re(line):
+    res = {}
+    while line:
+        m_at = fld_re.search(line)
+        if m_at:
+            fld_name, contents, rest = m_at.groups()
+            # res.setdefault(fld_name, set())
+            if fld_name not in res:
+                res[fld_name] = set()
+            res[fld_name].add(contents)
+            line = rest
+        else:
+            line = ''
+    return res
+
+
+def parse_fields_str(line):
+    res = {}
+    while line:
+        tok_start = line.find("[")
+        if tok_start < 0:
+            return res
+        tok_end = line.find("]")
+        if tok_end < 0:
+            return res
+        b = line[tok_start + 1:tok_end]
+        li = b.split(" ", 1)
+        if len(li) == 2:
+            fld, val = li
+            if val[0] == '"':
+                val = val[1:-1]
+            if fld not in res:
+                res[fld] = set()
+            res[fld].add(val)
+        else:
+            # logging.warning("unexpected number of fields: " + str(li))
+            pass
+        line = line[1+tok_end:]
+    return res
+
+
+parse_fields = parse_fields_str
+
+
+def set_parse_method(meth):
+    global parse_fields
+    if meth == "re":
+        parse_fields = parse_fields_re
+    logging.debug("parse_fields method: "+str(parse_fields))
